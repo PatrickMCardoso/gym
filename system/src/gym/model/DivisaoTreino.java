b4c31e1beb3cab61c48f45a0fc2b787f5969a4a3
PatrickMCardoso <patrick.cardoso@estudante.iftm.edu.br>
2024-05-07 10:50:49 -0300

    Adding Training Division class

---
+++ system/src/gym/model/DivisaoTreino.java
@@ -0,0 +1,65 @@
+package gym.model;
+
+import java.time.LocalDate;
+
+public class DivisaoTreino {
+    private int id;
+    private String nome;
+    private String descricao;
+    private LocalDate dataCriacao;
+    private LocalDate dataModificacao;
+
+    public DivisaoTreino(int id, String nome, String descricao, LocalDate dataCriacao, LocalDate dataModificacao) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.dataCriacao = dataCriacao;
+        this.dataModificacao = dataModificacao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public LocalDate getDataModificacao() {
+        return dataModificacao;
+    }
+
+    public void setDataModificacao(LocalDate dataModificacao) {
+        this.dataModificacao = dataModificacao;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + ", Nome: " + nome + ", Descrição: " + descricao +
+                ", Data de Criação: " + dataCriacao + ", Data de Modificação: " + dataModificacao;
+    }
+}
